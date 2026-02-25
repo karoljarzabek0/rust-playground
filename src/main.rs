92fd8f3cdfa9d3148ba8005d5d5bbdfc43dbc8ff
karoljarzabek0 <183915723+karoljarzabek0@users.noreply.github.com>
2026-02-25 13:43:38 +0100

    added aff file loading

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 // Libraries for the file reader
-use std::fs::File;
+use std::fs::{File, read};
+use std::hash::Hash;
 use std::io::{self, BufRead, Read};
 use std::path::Path;
 
 // For reading CLI args
-use std::env;
+use std::env::{self, current_exe};
 
 // For loading .dic and .aff files
 use std::collections::HashMap;
@@ -50,21 +51,81 @@ impl Dictionary {
 
 }
 
-struct Rules {
+
+#[derive(Debug)]
+struct AffFile {
+    groups: HashMap<char, RuleGroup>
+}
+
+#[derive(Debug)]
+struct RuleGroup {
     kind: String,
     flag: char,
     can_be_combined: bool,
-
-
+    rules: Vec<Rule>
 }
 
-
-
+#[derive(Debug)]
 struct Rule {
     remove: String,
     add: String,
     condition: String
 }
+
+impl AffFile {
+    pub fn new(aff_path: &str) -> Self {
+        Self { 
+            groups: Self::load(aff_path)
+        }
+    }
+    fn load(aff_path: &str) -> HashMap<char, RuleGroup> {
+        let mut rule_groups: HashMap<char, RuleGroup> = HashMap::new();
+
+        if let Ok(lines) = read_lines(aff_path) {
+            let mut current_flag_char: char = ' ';
+
+            for line in lines.flatten() {
+                let mut record: Vec<&str> = line.split(" ").collect();
+                record.retain(|word| word.len() >= 1);
+
+                if record.len() > 2 && (record[0] == "PFX" || record[0] == "SFX") {
+                    if record[2] == "Y" || record[2] == "N" {
+                        // Case when whe encouter the Rules header
+
+                        current_flag_char = record[1].chars().next().unwrap();
+
+                        rule_groups.entry(current_flag_char).or_insert(RuleGroup { 
+                            kind: record[0].to_string(),
+                            flag: record[1].to_string().chars().next().unwrap(),
+                            can_be_combined: record[2] == "Y",
+                            rules: vec![] }
+                        );
+                    } else {
+                        // Case when we encounter the individual Rule case
+                        match rule_groups.get_mut(&current_flag_char) {
+                            Some(rule_group) => rule_group.rules.push(Rule {
+                                remove: record[2].to_string(),
+                                add: record[3].to_string(),
+                                condition: record[4].to_string()
+                            }),
+                            None => panic!("Tried adding the rule out of order, sth is definetly wrong")
+                        }
+                    }
+                } else {
+                    current_flag_char = ' ';
+                }
+
+                println!("{:?}, group: {:?}", record, current_flag_char);
+
+                }
+            println!("{:?}", rule_groups);
+            
+            }
+
+        rule_groups
+        }
+}
+
 
 fn apply_rule(word: &str) {
 
@@ -80,9 +141,11 @@ fn main() {
     let aff_path: &str = &args[2];
     println!("Dict path:{}, Aff path: {}", dic_path, aff_path);
 
+    let aff_file = AffFile::new(aff_path);
+
     let dictionary = Dictionary::new(dic_path);
 
-let mut term = String::new();
+    let mut term = String::new();
     loop {
         term.clear();
         println!("-----------------------------------");
